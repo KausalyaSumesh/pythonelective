aa631341c60fee9ca4c432e5e40b6e99d02ac63b
KausalyaSumesh <kausalyasumesh03@gmail.com>
2025-01-19 23:07:46 +0530

    caesar_cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,22 @@
+message=input("enter the message: ")
+key=int(input("enter the key code: "))
+ciphertext=""
+for ch in message:
+    ordinal_ch=ord(ch)
+    ordinal_ch+=key
+    if ordinal_ch > ord('z'):
+        ordinal_ch=ord('a')+(ordinal_ch-ord('z'))
+    new_ch=chr(ordinal_ch)
+    ciphertext+=new_ch
+print(ciphertext)
+
+decrypted_message = ""
+
+for ch in ciphertext:
+    ordinal_ch = ord(ch)
+    ordinal_ch -= key
+    if ordinal_ch < ord('a'):
+        ordinal_ch = ord('z') - (ord('a') - ordinal_ch )
+    new_ch = chr(ordinal_ch)
+    decrypted_message += new_ch
+print("decrypted message:",decrypted_message)
