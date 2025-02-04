093204b1228cdcbd14db43d4cbff377f61b7f345
KausalyaSumesh <kausalyasumesh03@gmail.com>
2025-02-04 23:10:42 +0530

    list_programs

---
+++ large_small.py
@@ -0,0 +1,4 @@
+a = list(map(int,input().split()))
+print("list",a)
+print("The largest number is :",max(a))
+print("The smallest number is :",min(a))
---
+++ third_large.py
@@ -0,0 +1,3 @@
+a=[4,1,7,9,6,3,8]
+a.sort()
+print("Third largest number is ",a[-3])
